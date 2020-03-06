563c0288cc8c08b283eaee1c6aa37aca875fefbe
sxc562586657 <xingches@usc.edu>
2020-03-06 00:59:02 -0800

    Refactored and implemented time jumping feature

--- src/components/BaseMap/BaseMap.js
+++ src/components/BaseMap/BaseMap.js
@@ -1,10 +1,9 @@
 import React from "react";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./BaseMap.css";
 import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoic3hjNTYyNTg2NjU3IiwiYSI6ImNrNnZ4ZnVoMzA2c2kzbHJ4cmhxdzZ5ZWMifQ.NYqrc2_6uudQZgPnSTf1ZQ";
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
 class BaseMap extends React.Component {
   constructor(props) {
@@ -19,13 +18,13 @@ class BaseMap extends React.Component {
   componentDidMount() {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: "http://localhost:3000/styles/AD1783/style.json",
+      style: 'mapbox://styles/mapbox/streets-v11',
       minZoom: 2,
       maxZoom: 10
     });
-
     map.addControl(new mapboxgl.NavigationControl());
     map.addControl(new mapboxgl.GeolocateControl());
+    this.props.onMapObjectChange(map);
   }
 
   render() {
--- src/components/TimeSelector/TimeSelector.js
+++ src/components/TimeSelector/TimeSelector.js
@@ -1,21 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TimeSelector.css";
 
+const years = [
+  "BC3000",
+  "BC2030",
+  "BC1735",
+  "BC1300",
+  "BC1000",
+  "BC800",
+  "BC600",
+  "BC570",
+  "BC539",
+  "BC490",
+  "BC449",
+  "BC400",
+  "BC356",
+  "BC330",
+  "BC280",
+  "BC260",
+  "BC247",
+  "BC221",
+  "BC213",
+  "BC206",
+  "BC200",
+  "BC168",
+  "BC146",
+  "BC121",
+  "BC100",
+  "BC60",
+  "BC44",
+  "BC33",
+  "AD10",
+  "AD23",
+  "AD44",
+  "AD74",
+  "AD100",
+  "AD117",
+  "AD130",
+  "AD150",
+  "AD196",
+  "AD225",
+  "AD270",
+  "AD300",
+  "AD320",
+  "AD350",
+  "AD380",
+  "AD400",
+  "AD410",
+  "AD450",
+  "AD451",
+  "AD476",
+  "AD500",
+  "AD527",
+  "AD545",
+  "AD560",
+  "AD580",
+  "AD600",
+  "AD660",
+  "AD680",
+  "AD700",
+  "AD750",
+  "AD780",
+  "AD814",
+  "AD850",
+  "AD875",
+  "AD900",
+  "AD910",
+  "AD920",
+  "AD940",
+  "AD955",
+  "AD980",
+  "AD1000",
+  "AD1040",
+  "AD1080",
+  "AD1100",
+  "AD1127",
+  "AD1170",
+  "AD1200",
+  "AD1235",
+  "AD1300",
+  "AD1350",
+  "AD1400",
+  "AD1500",
+  "AD1599",
+  "AD1783",
+  "AD1812",
+  "AD1825"
+];
+
 function TimeSelector(props) {
-  var years = props.years;
+  const [currentYear, setCurrentYear] = useState("Select Year");
+  const onChangeYear = e => {
+    var selectedYear = e.currentTarget.textContent;
+    setCurrentYear(selectedYear);
+    var map = props.map;
+    map.setStyle(
+      props.styleTemplate.replace(
+        "{year}",
+        selectedYear.slice(-2) + selectedYear.slice(0, -2)
+      )
+    );
+    props.onMapObjectChange(map);
+  };
 
   return (
-    <div class="dropdown">
-      <button class="dropbtn" id="year-selection-button">
-        Select Year
+    <div className="dropdown">
+      <button className="dropbtn" id="year-selection-button">
+        {currentYear}
       </button>
-      <div class="dropdown-content" id="years">
+      <div className="dropdown-content" id="years">
         <ul>
           {years.map(year => (
-            <li class="normal-li cp">
-              <span class="inline">{year.slice(2)}</span>
-              <span class="ad-or-bc">{year.slice(0, 2)}</span>
+            <li className="normal-li cp" onClick={onChangeYear} key={year}>
+              <span className="inline">{year.slice(2)}</span>
+              <span className="ad-or-bc">{year.slice(0, 2)}</span>
             </li>
           ))}
         </ul>
